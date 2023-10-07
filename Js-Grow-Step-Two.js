9ccb646b31302725c611871a3662e461d50820e6
ckxprs <131081027+ckxprs@users.noreply.github.com>
2023-10-07 16:45:52 -0400

    Add files via upload

---
+++ Js-Grow-Step-Two.js
@@ -0,0 +1,13 @@
+// Assuming you have a form element with the ID 'your-form-id'
+var form = document.getElementById('6530561');
+var inputField = document.getElementById('109685892'); // Replace with your input ID
+
+// Add an event listener for the "submit" event on the form
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var inputValue = inputField.value;
+
+  // Redirect to another webpage with the captured value as a URL parameter
+  window.location.href = 'https://xprscapital.com/grow-step-two?unique_id=' + encodeURIComponent(inputValue);
+});
